37b932671005bf55039ba1903112b58b6a3a809f
NTUT_Tim <48685236+timtimno1@users.noreply.github.com>
2023-11-29 11:38:44 +0000

    Fix file bug

--- hw05/src/strong/17102594effa278a@19.js
+++ hw05/src/strong/17102594effa278a@19.js
@@ -240,7 +240,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["output(分數)@2.json", {url: new URL("./output(分數).json", import.meta.url), mimeType: "application/json", toString}]
+    ["output(分數).json", {url: new URL("../output(分數).json", import.meta.url), mimeType: "application/json", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
